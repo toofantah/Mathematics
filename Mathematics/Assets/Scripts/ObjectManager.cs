389cd9c330c6c61d3fa2e7dfaec0445b6ca78e83
toofantah <toofan.tah@gmail.com>
2022-01-24 01:05:45 +0300

    Update

---
+++ Mathematics/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    public GameObject objPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+       GameObject instance = Instantiate(objPrefab, new Vector3(Random.Range(-100, 100),
+            Random.Range(-100, 100), objPrefab.transform.position.z),
+            Quaternion.identity);
+
+        print("The fuel is at location of:" + instance.transform.position.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
