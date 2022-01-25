b99f90b9aa4303d318533dd739ea529601c601b3
toofantah <toofan.tah@gmail.com>
2022-01-25 22:30:19 +0300

    Update

--- Mathematics/Assets/Scripts/ObjectManager.cs
+++ Mathematics/Assets/Scripts/ObjectManager.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class ObjectManager : MonoBehaviour
 {
     public GameObject objPrefab;
+    public Vector3 objPosition;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
        GameObject instance = Instantiate(objPrefab, new Vector3(Random.Range(-100, 100),
             Random.Range(-100, 100), objPrefab.transform.position.z),
             Quaternion.identity);
+
+        objPosition = instance.transform.position;
 
         print("The fuel is at location of:" + instance.transform.position.ToString());
     }
--- Mathematics/Assets/Scripts/UIManager.cs
+++ Mathematics/Assets/Scripts/UIManager.cs
@@ -7,11 +7,23 @@ public class UIManager : MonoBehaviour
 {
     public GameObject tank;
     public GameObject fuel;
+    public Text tankposition;
+    public Text fuelPosition;
+    public Text energyAmount;
+
+    public void AddEnergy(string amont)
+    {
+        int n;
+        if(int.TryParse(amont, out n))
+            energyAmount.text = amont;
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tankposition.text = tank.transform.position.ToString();
+        fuelPosition.text = fuel.GetComponent<ObjectManager>().objPosition.ToString();
     }
 
     // Update is called once per frame
